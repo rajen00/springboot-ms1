3afa7ae08fc1710dd03229f306c55014cbcbc631
rajen00 <rajen.banerjee3112@gmail.com>
2019-05-07 08:44:29 +0530

    Initial commit

---
+++ src/main/java/com/jx/demo/springbootms1/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.jx.demo.springbootms1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
